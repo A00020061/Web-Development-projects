eaef9eaa685541d7855a53100b3a67acb1b62b5b
Sandeep-Kumar <sandeepkumar4405@gmail.com>
2022-04-02 23:59:28 +1030

    create practice file

---
+++ web development - Copy/Javascript/leetcodepractice.js
@@ -0,0 +1,12 @@
+// -------------------count 1 in 32 bit ]]]]]]]]]]]]
+// let a = 000001011;
+// let b = a.toString(2);
+// // let c = b.split('').filter(item=>item==1).length;
+// let c =(b.match(/1/g) || []).length;
+// console.log(c);
+
+// ---------------substract product and addition of the number----------
+
+let a = 10.8;
+let b = Math.floor(a);
+console.log(b);
